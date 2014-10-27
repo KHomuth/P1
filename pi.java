90fff43552e4d30731a0bae6410964e0c4b13a87
yellowKH <K-HP@hotmail.de>
2014-10-27 14:28:26 +0100

    Update pi.java

--- pi.java
+++ pi.java
@@ -2,12 +2,12 @@ public class pi{
 	public static void main(String[] args){
 
 
-   int num = -4;  //Zähler
+   double num = -4;  //Zähler
     double sum = 0;
    
 
 
- for (int denom = 1; denom <= 20; denom=denom+2){   //generiert den Nenner!
+ for (double denom = 1; denom <= 20; denom=denom+2){   //generiert den Nenner!
    num = num*(-1);
    sum = sum + (num/denom);
  }
