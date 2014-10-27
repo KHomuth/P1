377388257fc03c9605272deab6f2525f106ecedb
yellowKH <K-HP@hotmail.de>
2014-10-27 14:41:59 +0100

    Update pi.java

--- pi.java
+++ pi.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
+
 public class pi{
 	public static void main(String[] args){
 
+ System.out.print("Fuer wie viele Summanden wollen Sie pi berechnen? ");
+
+ Scanner pi = new Scanner(System.in);
+ long sumnum = pi.nextLong();
 
    double num = -4;  //Zähler
-    double sum = 0;
+   double sum = 0;
    
 
 
- for (double denom = 1; denom <= 20; denom=denom+2){   //generiert den Nenner!
+ for (double denom = 1; denom <= 100000000; denom=denom+2){   //generiert den Nenner!
    num = num*(-1);
    sum = sum + (num/denom);
  }
